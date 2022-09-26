93bf82111554030d59587ad0fbd0524020838d4c
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-09-26 14:13:25 -0300

    Buscando array de repositorios e montando flatlist

--- src/paginas/Repositorios/index.js
+++ src/paginas/Repositorios/index.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, FlatList, TouchableOpacity } from 'react-native';
 import estilos from './estilos';
+import { pegarRepositoriosDoUsuario } from '../../servicos/Requisicoes/repositorios';
 
 export default function Repositorios({ route, navigation }) {
     const [repo, setRepo] = useState([]);
+
+    useEffect(async () => {
+        const resultados = await pegarRepositoriosDoUsuario(route.params.id);
+        setRepo(resultados);
+
+    }, [])
 
     return (
         <View style={estilos.container}>
@@ -14,6 +21,21 @@ export default function Repositorios({ route, navigation }) {
                 >
                     <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
                 </TouchableOpacity>
+
+                <FlatList 
+                    data={repo}
+                    style={{width: '100%'}}
+                    keyExtractor={repo => repo.id}
+                    renderItem={({item}) => (
+                        <TouchableOpacity
+                            style={estilos.repositorio}
+                            onPress={() => navigation.navigate('InfoRepositorio', {item})}
+                        >
+                            <Text style={estilos.repositorioNome}>{item.name}</Text>
+                            <Text style={estilos.repositorioData}>{item.data}</Text>
+                        </TouchableOpacity>
+                    )}
+                />
         </View>
     );
 }
